0e979921454ff1a7bf3c7223f12cfea19108b0c8
이원기 <log19951@gmail.com>
2022-06-03 00:21:56 +0900

    Project Setting

---
+++ src/test/java/com/log1995/javacalculator/JavaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log1995.javacalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
