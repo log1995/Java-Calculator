972ac9fba885fa369749ded2be18cee8a30745f2
이원기 <log19951@gmail.com>
2022-06-09 18:37:27 +0900

    세 번째 리뷰 코드 수정   - 계산을 위한 enum 클래스 생성   - 각 계산에 우선도(priority)를 부여해서 곱하기/나누기를 우선적으로 처리   - 기존 ArrayList 대신 Stack읉 통해 수식 처리

--- src/main/java/com/log1995/javacalculator/service/CalculatorService.java
+++ src/main/java/com/log1995/javacalculator/service/CalculatorService.java
@@ -3,55 +3,56 @@ package com.log1995.javacalculator.service;
 
 import com.log1995.javacalculator.repository.CalculatorRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 
 public class CalculatorService {
 
     private CalculatorRepository calculatorRepository = new CalculatorRepository();
+    private Operator operator;
+
+    private static Stack<String> formulaStack = new Stack<>();
 
     public String calculateFormula(String formula){
 
         String[] formulaArr = formula.split("\\s+");
-        ArrayList<String> formulaList = new ArrayList<>();
 
-        for(String s : formulaArr){
-            formulaList.add(s);
-        }
+        formulaStack.push(formulaArr[0]);
+        double resultNum = 0;
+        String result;
+        int len = formulaArr.length;
 
-        calculate(formulaList, "*", "/");
-        calculate(formulaList, "+", "-");
+        for(int i = 1; i < len; i += 2){
+            formulaStack.push(formulaArr[i]);
+            formulaStack.push(formulaArr[i + 1]);
 
-        String result = formulaList.get(0);
-        calculatorRepository.saveResult(formula, result);
-        return result;
+            operator = Operator.initOperator(formulaArr[i]);
+            int priority = operator.getPriority();
 
-    }
-
-    public void calculate(List<String> list, String a, String b){
-
-        for(int i = 1; i <list.size(); i += 2){
-            String oper = list.get(i);
-
-            if(oper.equals(a) || oper.equals(b)) {
-                double second = Double.parseDouble(list.remove(i + 1));
-                list.remove(i);
-                double first = Double.parseDouble(list.remove(i - 1));
-
-                if(oper.equals("*")){
-                    list.add(i - 1, String.valueOf(first * second));
-                }else if(oper.equals("/")){
-                    list.add(i - 1, String.valueOf(first / second));
-                }else if(oper.equals("+")){
-                    list.add(i - 1, String.valueOf(first + second));
-                }else if(oper.equals("-")){
-                    list.add(i - 1, String.valueOf(first - second));
-                }
-                i -= 2;
+            if(priority == 1){
+                double second = Double.parseDouble(formulaStack.pop());
+                formulaStack.pop();
+                double first = Double.parseDouble(formulaStack.pop());
+                resultNum = operator.calculate(first, second);
+                formulaStack.push(String.valueOf(resultNum));
             }
         }
+
+        if(formulaStack.size() != 1){
+            int stackSize = formulaStack.size();
+            for(int i = 1; i < stackSize; i += 2){
+                double second = Double.parseDouble(formulaStack.pop());
+                operator = Operator.initOperator(formulaStack.pop());
+                double first = Double.parseDouble(formulaStack.pop());
+                resultNum = operator.calculate(first, second);
+                formulaStack.push(String.valueOf(resultNum));
+            }
+        }
+
+        result = formulaStack.pop();
+        calculatorRepository.saveResult(formula, result);
+        return result;
     }
 
     public Map<String, String> getData(){
